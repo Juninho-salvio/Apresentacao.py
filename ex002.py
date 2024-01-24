8b4567493b782952126b87caefe1bf2fdb782300
Sálvio De Oliveira Santos Júnior <79759829+Juninho-salvio@users.noreply.github.com>
2024-01-24 17:45:54 -0300

    Add files via upload

---
+++ ex002.py
@@ -0,0 +1,5 @@
+nome = input('Digite seu nome: ')
+print('É um prazer te conhercer,',nome,'!')
+print('É um prazer te conhercer \033[;34m{}\033[m!' .format(nome))
+
+print('É um prazer te conhercer,\033[1;31m {}\033[m, voce  é muito bonito' .format(nome))
